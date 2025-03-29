c3b69945ca711580f0eacc00dc78b8b97a599c41
Nikita <oblivorten@gmail.com>
2025-03-29 22:34:07 +0300

    Добавил создание новой задачи

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from .views import task_list
+from .views import task_list, task_create
 
 urlpatterns = [
     path('tasks/', task_list, name='tasks'),
+    path('task_create/', task_create, name='task_create'),
 ]
